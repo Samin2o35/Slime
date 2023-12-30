17653a155bbefaa5091a6e44894cd1f5ababda9c
Samin <saminmohammed65@gmail.com>
2023-12-30 17:57:10 +0600

    WIP camera shake via Code

--- Assets/Scripts/Environment/DebrisHealth.cs
+++ Assets/Scripts/Environment/DebrisHealth.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class NewBehaviourScript : MonoBehaviour, IDamageable
 {
     [SerializeField] private float maxHealth;
     private float currentHealth;
     public bool HasTakenDamage { get; set; }
+    private CinemachineImpulseSource impulseSource;
 
     private void Start()
     {
         currentHealth = maxHealth;
+
+        impulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
     public void Damage(float damageAmount)
     {
+        CameraShakeManager.instance.CameraShake(impulseSource);
         HasTakenDamage = true;
         currentHealth -= damageAmount;
         if (currentHealth <= 0)
---
+++ Assets/Scripts/Managers/CameraShakeManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShakeManager : MonoBehaviour
+{
+    public static CameraShakeManager instance;
+    private CinemachineImpulseDefinition impulseDefinition;
+    [SerializeField] private float globalShakeForce;
+    [SerializeField] private CinemachineImpulseListener impulseListener;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void CameraShake(CinemachineImpulseSource impulseSource)
+    {
+        impulseSource.GenerateImpulseWithForce(globalShakeForce);
+    }
+
+    public void ScreenShakeFormProfile(ScreenShakeProfile profile, CinemachineImpulseSource impulseSource)
+    {
+        //apply settings from scriptable object
+        SetupScreenShakeSettings(profile, impulseSource);
+        //screenshake
+        impulseSource.GenerateImpulseWithForce(profile.impactForce);
+    }
+
+    private void SetupScreenShakeSettings(ScreenShakeProfile profile, CinemachineImpulseSource impulseSource)
+    {
+        impulseDefinition = impulseSource.m_ImpulseDefinition;
+        
+        //Change Impulse Source Settings
+        impulseDefinition.m_ImpulseDuration = profile.impactTime;
+        impulseSource.m_DefaultVelocity = profile.defaultVelocity;
+        impulseDefinition.m_CustomImpulseShape = profile.impulseCurve;
+        
+        //Change Impulse Listener Settings
+        impulse
+    }
+}
