4efe352dc3b15194c6682918f00a889be19d2582
Samin <saminmohammed65@gmail.com>
2024-01-26 16:13:58 +0600

    Removed Redundant scripts
    Removed Environment related scripts i.e. Debris Health
    Removed IEnemyDamageable
    IEnemyDamageable interface is handled by Idamageable

--- Assets/Scripts/Enemy/Interfaces/IDamageable.cs
+++ Assets/Scripts/Enemy/Interfaces/IDamageable.cs
@@ -7,4 +7,11 @@ public interface IDamageable
     public void Damage(float damageAmount, Vector2 attackDirection);
 
     public bool HasTakenDamage { get; set; }
+
+    // take from IEnemyDamageable
+
+    public void EnemyDamage(float eDamageAmount);
+
+    public bool HasTakenDamage { get; set; }
+    public bool HasTakenDamage { get; set; }
 }
